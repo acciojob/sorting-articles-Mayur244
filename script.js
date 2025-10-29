3ad22fa4dc793275d2d00ac6bef530b5054e0ff4
Mayur Ghodaskar <97151293+Mayur244@users.noreply.github.com>
2025-10-29 21:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function removeArticles(band) {
+	let reg = /^(a |an |the )/i;
+	return band.replace(reg, "");
+}
+
+const sortedBands = bands.slice().sort((a, b) => {
+	const bandA = removeArticles(a).toLowerCase();
+	const bandB = removeArticles(b).toLowerCase();
+
+	if(bandA < bandB) return -1;
+	if(bandA > bandB) return 1;
+	return 0;
+})
+
+const list = document.getElementById("band");
+
+sortedBands.forEach((band) => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	list.appendChild(li);
+})
